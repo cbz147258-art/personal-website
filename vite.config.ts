cab2ce762a9e7b1b27f1ba631820f253b4597709
cbz147258-art <cbz147258-art@users.noreply.github.com>
2026-06-06 14:20:13 +0800

    fix: 修改 base 路径为 /，适配根域名部署

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite"
+﻿import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import UnoCSS from "unocss/vite"
 
 export default defineConfig({
-  base: "/personal-website/",
+  base: "/",
   plugins: [vue(), UnoCSS()],
 })
